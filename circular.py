1872df6f884553aae0716b482048375e8677e85b
Jamie Saxon <jsaxon@uchicago.edu>
2016-10-19 04:12:10 -0500

    fourth homework

---
+++ circular.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+
+# at the end your answer
+# should be in solution
+solution = 0
+
+
+# First calculate the 
+# Sieve of Eratosthenes
+# up to a million.
+
+
+# Now loop over those primes, 
+# And ask in each case if it's 
+# a circular prime
+
---
+++ q3.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python 
+
+# solution should hold the final answer
+solution = 0
+
