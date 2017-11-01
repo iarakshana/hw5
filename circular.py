e5fc7d4feb021b2b9a0a2e6b799fb6f9f67d97c1
Immanuel Adriana Rakshana <iarakshana@uchicago.edu>
2017-11-01 01:28:52 -0500

    all done

--- circular.py
+++ circular.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python
 
 def isprime(n):
-    # make sure n is a positive integer
+    #make sure n is a positive integer
     n = abs(int(n))
-    # 0 and 1 are not primes
+    #0 and 1 are not primes
     if n < 2:
         return False
-    # 2 is the only even prime number
     if n == 2:
         return True
-    # all other even numbers are not primes
+    #every other even number is not prime
     if not n & 1:
         return False
-    # range starts with 3 and only needs to go up the squareroot of n
-    # for all odd numbers
+    #range starts with 3 and only needs to go up the squareroot of nfor all odd numbers
     for x in range(3, int(n ** 0.5) + 1, 2):
         if n % x == 0:
             return False
     return True
 
-def new_rotation(y, n):
+def newrotation(y, n):
     #Checks the rotation of each prime number for primes
     c = 0
-    while n != c:
+    while n != c:#everything but 0
         j = y[1:]
         r = y[0]
         j.append(r)
@@ -37,16 +35,16 @@ def new_rotation(y, n):
 
 top = 1000000
 def main():
-    L = []
+    R = []
     for n in range(10, top):
         if isprime(n):
             y = list(str(n))
-            if new_rotation(y, n) == "True":
-                L.append(n)
+            if newrotation(y, n) == "True":
+                R.append(n)
             else:
                 continue
     # add 4 to the total (manually add prime 2, 3, 5, and 7)
-    solution = (len(L) + 4)
+    solution = (len(R) + 4)
     print(solution)
 
 if __name__ == '__main__':
